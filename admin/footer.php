5123d846d89093346e18dc9f24acb4b015fa75d6
obhy <obhystudio@gmail.com>
2020-01-17 04:20:07 +0700

    Add files via upload

---
+++ admin/footer.php
@@ -0,0 +1,19 @@
+<?php
+if (!defined("ADMINPATH")) {
+    exit;
+}
+
+echo '
+			</div>
+		</div>
+		<footer>
+			<div class="container-fluid">
+				<p class="copyright juicycodes">
+					Designed & Crafted With <i class="fa fa-heart heart"></i> by <a href="https://stci.cl">stci.cl</a>.
+				</p>
+			</div>
+		</footer>
+	</div>
+</div>
+';
+$html->GetFooter();
